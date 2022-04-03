7ce56c1d5a9806faa383325cc2b669734e97f47b
emilynak <emilynak@gmail.com>
2022-04-03 17:47:07 -0300

    second version

---
+++ scripts.js
@@ -0,0 +1,56 @@
+let Bebida;
+let Sobremesa;
+let Prato;
+
+
+function selecionarPrato(nomePrato) {
+    
+  const botaoSelecionado = document.querySelector(".selecionado");
+  if (botaoSelecionado !== null) {
+    //REMOVE A CLASSE SELECIONADO
+    botaoSelecionado.classList.remove("selecionado");
+  }
+
+  //ADICIONO A CLASSE SELECIONADO
+  nomePrato.classList.add("selecionado");
+  Prato = nomePrato.innerHTML;
+
+};
+
+function selecionarBebida(nomeBebida) {
+  const botaoSelecionado = document.querySelector(".bebidas").querySelector(".selecionado");
+  if(botaoSelecionado !== null) {
+    botaoSelecionado.classList.remove("selecionado")
+  }
+  nomeBebida.classList.add("selecionado");
+  Bebida = nomeBebida.innerHTML;
+};
+
+function selecionarSobremesa(nomeSobremesa) {
+  const botaoSelecionado = document.querySelector(".sobremesas").querySelector(".selecionado");
+  if(botaoSelecionado !== null) {
+    botaoSelecionado.classList.remove("selecionado")
+  }
+  nomeSobremesa.classList.add("selecionado");
+  Sobremesa = nomeSobremesa.innerHTML;
+
+  if (Prato && Bebida && Sobremesa) {
+    document.querySelector(".button").classList.add("botaoVerde");
+    document.querySelector(".button").innerHTML = "Fechar pedido"
+  } 
+  
+};
+
+function finalizarPedido() {
+  if (Prato && Bebida && Sobremesa) {
+    
+    let mensagem = 
+    `Olá, gostaria de fazer o pedido:
+    - Prato: Frango Yin Yang
+    - Bebida: Coquinha Gelada
+    - Sobremesa: Pudim
+    Total: R$ 27.70`
+    mensagem = window.encodeURIComponent(mensagem);
+    window.open("https://wa.me/5542984438733?text=" + mensagem);
+  } 
+}
