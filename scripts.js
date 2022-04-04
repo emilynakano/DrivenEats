8eab275d9909ade7392c14f29af379eab1de7a53
emilynak <emilynak@gmail.com>
2022-04-04 11:05:32 -0300

    last version

--- scripts.js
+++ scripts.js
@@ -8,33 +8,41 @@ function selecionarPrato(nomePrato) {
   const botaoSelecionado = document.querySelector(".selecionado");
   if (botaoSelecionado !== null) {
     //REMOVE A CLASSE SELECIONADO
+    document.querySelector(".selecionado .vector").classList.add("escondido");
     botaoSelecionado.classList.remove("selecionado");
-    Document.querySelector(".vector").classList.add("escondido")
+    
+
   }
 
   //ADICIONA A CLASSE SELECIONADO
   nomePrato.classList.add("selecionado");
   Prato = nomePrato.innerHTML;
-  Document.querySelector(".vector").classList.remove("escondido")
+  document.querySelector(".selecionado .vector").classList.remove("escondido");
 
 };
 
 function selecionarBebida(nomeBebida) {
   const botaoSelecionado = document.querySelector(".bebidas").querySelector(".selecionado");
   if(botaoSelecionado !== null) {
+    document.querySelector(".bebidas .selecionado .vector").classList.add("escondido");
     botaoSelecionado.classList.remove("selecionado")
   }
   nomeBebida.classList.add("selecionado");
   Bebida = nomeBebida.innerHTML;
+  document.querySelector(".bebidas .selecionado .vector").classList.remove("escondido");
+
 };
 
 function selecionarSobremesa(nomeSobremesa) {
   const botaoSelecionado = document.querySelector(".sobremesas").querySelector(".selecionado");
   if(botaoSelecionado !== null) {
+    document.querySelector(".sobremesas .selecionado .vector").classList.add("escondido");
     botaoSelecionado.classList.remove("selecionado")
   }
   nomeSobremesa.classList.add("selecionado");
   Sobremesa = nomeSobremesa.innerHTML;
+  document.querySelector(".sobremesas .selecionado .vector").classList.remove("escondido");
+
 
   if (Prato && Bebida && Sobremesa) {
     document.querySelector(".button").classList.add("botaoVerde");
@@ -53,6 +61,6 @@ function finalizarPedido() {
     - Sobremesa: Pudim
     Total: R$ 27.70`
     mensagem = window.encodeURIComponent(mensagem);
-    window.open("https://wa.me/5542984438733?text=" + mensagem);
+    window.open("https://wa.me/5542984172265?text=" + mensagem);
   } 
 }
